fb281fffc1e5bce6dbafcf5621d02315522990f4
Magda Danielyan <mdanielyan50@gmail.com>
2025-11-12 10:07:32 +0100

    header component added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
+    <AppHeader />
+
     <BaseCard />
   </div>
 </template>
 
 <script>
 import BaseCard from "./components/BaseCard.vue";
-import {stockService} from "@/services/stockService";
+import {stockService} from "./services/stockService";
+import AppHeader from "./components/AppHeader.vue";
 export default {
   name: "App",
-  components: {BaseCard},
+  components: {AppHeader, BaseCard},
 
   async created() {
     this.data = await stockService.fetchData("$AAPL");
@@ -19,10 +22,19 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: Rubik;
+  src: url("@/assets/fonts/Rubik-Italic-VariableFont_wght.ttf") format("ttf");
+  src: url("@/assets/fonts/Rubik-VariableFont_wght.ttf") format("ttf");
+  font-weight: normal;
+  font-style: normal;
+}
+
 body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: Rubik, sans-serif;
 }
 #app {
   background: rgba(1, 31, 53, 1);
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <svg
+      width="120"
+      height="60"
+      viewBox="0 0 96 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0 0H80C88.8366 0 96 7.16344 96 16C96 24.8366 88.8366 32 80 32H0V0Z"
+        fill="#39DAFF"
+      />
+    </svg>
+    <h1>The Magnificent Seven Companies</h1>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped>
+header {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  padding: 30px;
+  color: rgba(249, 249, 249, 1);
+  font-size: 36px;
+  font-weight: 500;
+}
+</style>
